6670ae52eefb30bb94b1e360bfc0e1b73b5482f2
vloss <viniciusvianaloss@gmail.com>
2023-01-23 11:59:48 -0300

    Criação de todas telas com elementos necessários.

---
+++ Compras USA/Compras USA/TaxesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaxesViewController.swift
+//  Compras USA
+//
+//  Created by Vinicius Loss on 23/01/23.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
