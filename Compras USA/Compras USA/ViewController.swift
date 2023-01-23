0346353c85f9aefaf64b150c56d57c762134e43e
vloss <viniciusvianaloss@gmail.com>
2023-01-23 11:02:53 -0300

    Criação do projeto.

---
+++ Compras USA/Compras USA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compras USA
+//
+//  Created by Vinicius Loss on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
